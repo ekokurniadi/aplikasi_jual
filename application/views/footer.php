3a70b8b50b223417fd047548ae183720dbf1b2d0
ekokurniadi02 <ekokurniadi.02@gmail.com>
2021-03-25 17:20:14 +0700

    upload

--- application/views/footer.php
+++ application/views/footer.php
@@ -35,6 +35,21 @@
  <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
 <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
 <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
+
+
+<script>
+$(function() {
+  $('input[name="datetimes"]').daterangepicker({
+    timePicker: true,
+    startDate: moment().startOf('hour'),
+    endDate: moment().startOf('hour').add(32, 'hour'),
+    locale: {
+      format: 'M/DD hh:mm A'
+    }
+  });
+});
+</script>
+
   <script>
   // In your Javascript (external .js resource or <script> tag)
   $(document).ready(function() {
